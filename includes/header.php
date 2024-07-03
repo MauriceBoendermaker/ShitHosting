eaf20a5fd14a5d4deaa3cbbc34fa0808a641139a
Maurice <mauriceboendermaker@gmail.com>
2024-07-03 14:16:29 +0200

    Created header include

---
+++ includes/header.php
@@ -0,0 +1,61 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>ShitHosting - No Service Provider</title>
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+
+	<!-- Favicons -->
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1">
+	<link rel="manifest" href="/site.webmanifest?v=1">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#5bbad5">
+	<link rel="shortcut icon" href="/favicon.ico?v=1">
+	<meta name="apple-mobile-web-app-title" content="ShitHosting - Your No Nonsense Webhost">
+	<meta name="application-name" content="ShitHosting - Your No Nonsense Webhost">
+	<meta name="msapplication-TileColor" content="#2b5797">
+	<meta name="theme-color" content="#ff0000">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://www.shithosting.net/" />
+	<meta property="og:title" content="ShitHosting - Your No Nonsense Webhost" />
+	<meta property="og:description" content="ShitHosting - Your No Nonsense Webhost" />
+	<meta property="og:image" content="" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://www.shithosting.net/" />
+	<meta property="twitter:title" content="ShitHosting - Your No Nonsense Webhost" />
+	<meta property="twitter:description" content="ShitHosting - Your No Nonsense Webhost" />
+	<meta property="twitter:image" content="" />
+
+	<!-- FontAwesome Icons -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+	      integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+	<!-- Primary font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+	<!-- Bootstrap v5 -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+	<!-- PopperJS -->
+<!--	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"-->
+<!--	        integrity="sha512-6UofPqm0QupIL0kzS/UIzekR73/luZdC6i/kXDbWnLOJoqwklBK6519iUnShaYceJ0y4FaiPtX/hRnV/X/xlUQ=="-->
+<!--	        crossorigin="anonymous" referrerpolicy="no-referrer"></script>-->
+
+	<!-- Flickity carousel -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.pkgd.js" rel="script" type="text/javascript"></script>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.css" rel="stylesheet" type="text/css">
+
+	<!-- Flickity fullscreen package -->
+	<link rel="stylesheet" href="https://unpkg.com/flickity-fullscreen@2/fullscreen.css">
+	<script src="https://unpkg.com/flickity-fullscreen@2/fullscreen.js"></script>
+</head>
+
+<?php include "navbar.php" ?>
